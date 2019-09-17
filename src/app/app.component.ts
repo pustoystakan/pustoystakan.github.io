dcf7475e032cfd4c7538de51e7d7851e20c0e516
Stepan S. Sickar <mkoty@yandex.ru>
2019-09-17 23:33:33 +0300

    init logo page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PustoyStakan';
+  showElement = false;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.showElement = true;
+    }, 1000);
+  }
 }
