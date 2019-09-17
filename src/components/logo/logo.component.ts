5ad90563ebbaab40990c071812d366ac781fccb3
Stepan S. Sickar <mkoty@yandex.ru>
2019-09-18 01:56:49 +0300

    Update logo animation

---
+++ src/components/logo/logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  templateUrl: '../../assets/logo_dashed_rounded_2.svg',
+  styleUrls: ['./logo.component.scss']
+})
+export class LogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
