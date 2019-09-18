13882815ee811568ed8f46d382409a6e3f9fb649
Stepan S. Sickar <mkoty@yandex.ru>
2019-09-18 03:01:45 +0300

    change to previous logo variant

--- src/components/logo/logo.component.ts
+++ src/components/logo/logo.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
-  templateUrl: '../../assets/logo_normal_2.svg',
+  templateUrl: '../../assets/logo_dashed_rounded_2.svg',
   styleUrls: ['./logo.component.scss']
 })
 export class LogoComponent implements OnInit {
